71282e6a77610be7f2185e2c0b3a8dae2f532083
name <hegedis.andor@gmail.com>
2021-07-21 20:25:00 +0200

    use addComma in formatAuthorToLink componenet

--- components/FormatAuthorToLink.js
+++ components/FormatAuthorToLink.js
@@ -1,7 +1,6 @@
+import { addComma } from "../utils/addComma";
 import { formatAuthor } from "../utils/formatAuthor";
 import { default as Link } from "./StyledLink";
-
-const addComma = (name, index) => (index > 0 ? `, ${name}` : name);
 
 const FormatAuthorToLink = ({ author }) => {
   return formatAuthor(author).map(([name, githubUserName], index) => {
