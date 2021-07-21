d659bfe093c1a81e1c01dd272071ff8a04298dba
name <hegedis.andor@gmail.com>
2021-07-21 18:23:45 +0200

    add metadata header and title to sip page

--- pages/sips/[sip].js
+++ pages/sips/[sip].js
@@ -1,13 +1,34 @@
 import ReactMarkdown from "react-markdown";
 import CodeBlock from "../../components/CodeBlock";
-import { MARKDOWN_METADATA_FIELDS } from "../../utils/constants";
+import ProposalMetadataHeader from "../../components/ProposalMetadataHeader";
+import { MARKDOWN_METADATA_FIELDS, SIPS_FOLDER_PATH } from "../../utils/constants";
 import markdownToHtml, { getAllMarkdowns, getMarkdownBySlug } from "../../utils/markdown";
-
-const SIPS_FOLDER_PATH = "content/SIPs";
+import styles from "./sips.module.scss";
+import Image from "next/image";
+import { default as Link } from "../../components/StyledLink";
 
 const SipPage = ({ sip }) => {
   return (
     <>
+      <h1>
+        {`SIP-${sip.sip}: ${sip.title}`}
+        <Link href="" rel="nofollow">
+          {" "}
+          <Image src="/outline_link_black_24dp.png" alt="source" width={24} height={24} />
+        </Link>
+      </h1>
+
+      <div className={styles.tableContainer}>
+        <ProposalMetadataHeader
+          author={sip.author}
+          discussionsTo={sip["discussions-to"]}
+          status={sip.status}
+          created={sip.created}
+          updated={sip.updated}
+          requires={sip.requires}
+        />
+      </div>
+
       <ReactMarkdown components={CodeBlock} skipHtml>
         {sip.contentMarkdown}
       </ReactMarkdown>
@@ -18,8 +39,7 @@ const SipPage = ({ sip }) => {
 export default SipPage;
 
 export function getStaticProps({ params }) {
-  const slug = `sip-${params.sip}`;
-  const sip = getMarkdownBySlug(slug, SIPS_FOLDER_PATH, MARKDOWN_METADATA_FIELDS);
+  const sip = getMarkdownBySlug(params.sip, SIPS_FOLDER_PATH, MARKDOWN_METADATA_FIELDS);
   const contentHtml = markdownToHtml(sip.content || "");
 
   return {
@@ -41,7 +61,7 @@ export function getStaticPaths() {
   return {
     paths: proposalsNumber.map((proposal) => ({
       params: {
-        sip: proposal.sip.toString(),
+        sip: `sip-${proposal.sip.toString()}`,
       },
     })),
     fallback: false,
